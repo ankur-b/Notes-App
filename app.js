6f9fecbbee4f7c70931db4b3c5fdebff2fe7f2fd
ankur-b <ankurbarve29@gmail.com>
2023-02-05 20:24:00 +0530

    getting input from users

---
+++ app.js
@@ -0,0 +1,12 @@
+const getNotes = require("./notes")
+const chalk = require('chalk')
+
+const command = process.argv[2]
+
+console.log(process.argv)
+
+if(command === "add"){
+    console.log("Adding note!")
+}else if(command === "remove"){
+    console.log("Removing note!")
+}
